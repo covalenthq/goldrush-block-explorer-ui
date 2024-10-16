c5f3f20c1f882306bbccc481720cdeb97c8fe24f
Yashvardhan Jagnani <60016972+jagnani73@users.noreply.github.com>
2024-10-17 02:28:17 +0530

    fix: chain id -> name (#10)
    * fix: chain id -> name
    
    * fix: latest tx actionable address
    
    * feat(/): branding banner
    
    * feat: ui ux
    
    * feat: add delta for native token price

--- app/(goldrush)/[chain_id]/page.tsx
+++ app/(goldrush)/[chain_id]/page.tsx
@@ -5,12 +5,34 @@ import {
     GasCard,
     LatestBlocks,
     LatestTransactions,
+    useGoldRush,
 } from "@covalenthq/goldrush-kit";
 import Link from "next/link";
 
 const Home: React.FC<HomePageProps> = ({ params: { chain_id } }) => {
+    const { selectedChain } = useGoldRush();
+
     return (
         <main className="gbk-flex gbk-w-full gbk-flex-col gbk-gap-4">
+            <div className="gbk-text-center gbk-flex gbk-items-center gbk-gap-y-2 gbk-flex-col gbk-py-4">
+                <h3 className="gbk-text-4xl gbk-font-medium gbk-capitalize">
+                    GoldRush {selectedChain?.name?.replaceAll("-", " ")}{" "}
+                    Explorer
+                </h3>
+
+                <p className="text-secondary-light dark:text-secondary-dark gbk-font-bold">
+                    Seamless Cross-Chain Exploration, Powered by{" "}
+                    <a
+                        href="http://goldrush.dev"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="hover:gbk-underline gbk-transition-all"
+                    >
+                        GoldRush
+                    </a>
+                </p>
+            </div>
+
             <GasCard chain_name={chain_id} />
 
             <div className="gbk-grid gbk-grid-cols-1 gbk-gap-8 xl:gbk-grid-cols-2">
@@ -39,19 +61,10 @@ const Home: React.FC<HomePageProps> = ({ params: { chain_id } }) => {
                             chain_name={chain_id}
                             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
                             // @ts-expect-error
-                            actionable_from={(from__address) => ({
+                            actionable_address={(from__address) => ({
                                 parent: Link,
                                 parentProps: {
                                     href: `/${chain_id}/address/${from__address}`,
-                                    className: "hover:underline",
-                                },
-                            })}
-                            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                            // @ts-expect-error
-                            actionable_to={(to__address) => ({
-                                parent: Link,
-                                parentProps: {
-                                    href: `/${chain_id}/address/${to__address}`,
                                     className: "hover:underline",
                                 },
                             })}
--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import { goldrushConfig } from "@/goldrush.config";
+import { timestampParser } from "@/utils/functions";
 import { useDebounce } from "@/utils/hooks";
-import { type ChainItem } from "@covalenthq/client-sdk";
-import { ChainSelector, useGoldRush } from "@covalenthq/goldrush-kit";
+import { type Price, type Chain, type ChainItem } from "@covalenthq/client-sdk";
+import {
+    ChainSelector,
+    Timestamp,
+    useGoldRush,
+} from "@covalenthq/goldrush-kit";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound, useParams, usePathname, useRouter } from "next/navigation";
@@ -17,18 +22,24 @@ export const Navbar: React.FC = () => {
         searchHandler,
         updateThemeHandler,
         theme,
+        goldrushClient,
     } = useGoldRush();
 
     const { push } = useRouter();
     const path = usePathname();
-    const { chain_id } = useParams<{ chain_id: string }>();
+    const { chain_id } = useParams<{
+        chain_id: string;
+    }>();
 
     const [searchInput, setSearchInput] = useState<string>("");
     const [open, setOpen] = useState<boolean>(false);
+    const [nativePrice, setNativePrice] = useState<Price | null>(null);
+    const [delta, setDelta] = useState<number | null>(null);
+
     useEffect(() => {
         if (!chains) return;
 
-        const _chains: ChainItem[] = goldrushConfig.chains.length
+        const _whitelistedChains: ChainItem[] = goldrushConfig.chains.length
             ? goldrushConfig.chains.reduce((acc: ChainItem[], nameOrId) => {
                   const foundChain: ChainItem | null =
                       chains.find(
@@ -44,11 +55,13 @@ export const Navbar: React.FC = () => {
             : chains;
 
         if (!chain_id) {
-            changeSelectedChainHandler(_chains[0]);
+            changeSelectedChainHandler(_whitelistedChains[0], true);
         } else {
             const chain: ChainItem | null =
-                _chains.find(
-                    (chain) => chain?.chain_id?.toString() === chain_id
+                _whitelistedChains.find(
+                    (chain) =>
+                        chain?.name === chain_id ||
+                        chain?.chain_id?.toString() === chain_id.toString()
                 ) ?? null;
             if (chain) {
                 changeSelectedChainHandler(chain);
@@ -56,20 +69,81 @@ export const Navbar: React.FC = () => {
                 notFound();
             }
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [chains, chain_id]);
 
-    const changeSelectedChainHandler = useCallback(
-        (chain: ChainItem) => {
-            if (chain?.chain_id) {
-                const paths = path.split("/");
-                paths.shift();
-                paths[0] = chain.chain_id.toString();
-                setSelectedChain(chain);
-                push(`/${paths.join("/")}`);
+    useEffect(() => {
+        (async () => {
+            try {
+                if (!chain_id) {
+                    return;
+                }
+
+                const today = new Date();
+                const yesterday = new Date(today);
+                yesterday.setDate(today.getDate() - 1);
+
+                setNativePrice(null);
+                const { data, ...error } =
+                    await goldrushClient.PricingService.getTokenPrices(
+                        chain_id as Chain,
+                        "USD",
+                        "0x0000000000000000000000000000000000000000",
+                        {
+                            from: timestampParser(
+                                today.toISOString(),
+                                "YYYY MM DD"
+                            ),
+                            to: timestampParser(
+                                yesterday.toISOString(),
+                                "YYYY MM DD"
+                            ),
+                        }
+                    );
+
+                if (error.error) {
+                    throw error;
+                }
+
+                if (data?.[0]?.items?.[0]) {
+                    setNativePrice(data[0].items[0]);
+                }
+
+                if (
+                    data?.[0]?.items?.[0]?.price &&
+                    data?.[0]?.items?.[1]?.price
+                ) {
+                    const todayPrice = data[0].items[0].price;
+                    const yesterdayPrice = data[0].items[1].price;
+                    setDelta(
+                        +(
+                            ((todayPrice - yesterdayPrice) / yesterdayPrice) *
+                            100
+                        ).toFixed(2)
+                    );
+                }
+            } catch (error) {
+                console.error(error);
             }
-        },
-        [path, push, setSelectedChain]
-    );
+        })();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [chain_id]);
+
+    const changeSelectedChainHandler = (
+        chain: ChainItem,
+        redirect: boolean = false
+    ) => {
+        if (!chain?.name) {
+            return;
+        }
+        setSelectedChain(chain);
+        if (redirect || chain_id?.length === 1) {
+            const paths = path.split("/");
+            paths.shift();
+            paths[0] = chain.name;
+            push(`/${paths.join("/")}`);
+        }
+    };
 
     useDebounce(
         () => {
@@ -101,48 +175,76 @@ export const Navbar: React.FC = () => {
                     push(`/not-found`);
                     return;
             }
-            push(`/${chain.chain_id}/${page}/${input}`);
+            push(`/${chain.name}/${page}/${input}`);
         },
         [push, searchHandler]
     );
 
     return (
-        <nav className="bg-background-light text-foreground-light dark:bg-background-dark dark:text-foreground-dark border-secondary-light dark:border-secondary-dark gbk-sticky gbk-left-0 gbk-top-0 gbk-z-50 gbk-flex gbk-w-full gbk-flex-wrap gbk-items-center gbk-justify-between gbk-gap-x-4 gbk-border-b gbk-px-8 gbk-py-4 md:gbk-flex-nowrap">
-            <Link
-                href={`/${selectedChain?.chain_id}`}
-                className="gbk-mr-auto gbk-flex gbk-w-fit gbk-items-center gbk-gap-2"
-            >
-                <figure className="gbk-relative gbk-h-10 gbk-w-10">
-                    <Image
-                        src={goldrushConfig.brand.logo_url}
-                        alt={`GoldRush Block Explorer - ${selectedChain?.label}`}
-                        fill
-                        className="gbk-object-cover"
-                    />
-                </figure>
+        <nav className="bg-background-light text-foreground-light dark:bg-background-dark dark:text-foreground-dark border-secondary-light dark:border-secondary-dark gbk-sticky gbk-left-0 gbk-top-0 gbk-z-50 gbk-flex gbk-w-full gbk-flex-wrap gbk-items-center gbk-justify-between gbk-gap-x-4 gbk-border-b gbk-px-8 gbk-py-4 lg:gbk-flex-nowrap">
+            <div className="gbk-flex gbk-items-center gbk-gap-x-8">
+                <Link
+                    href={`/${selectedChain?.name}`}
+                    className="gbk-mr-auto gbk-flex gbk-w-fit gbk-items-center gbk-gap-2"
+                >
+                    <figure className="gbk-relative gbk-h-10 gbk-w-10">
+                        <Image
+                            src={goldrushConfig.brand.logo_url}
+                            alt={`GoldRush Block Explorer - ${selectedChain?.label}`}
+                            fill
+                            className="gbk-object-cover"
+                        />
+                    </figure>
 
-                <h1 className="gbk-whitespace-nowrap gbk-text-lg gbk-font-medium gbk-leading-none">
-                    {goldrushConfig.brand.title}
-                    <br />
-                    {goldrushConfig.brand.subtitle}
-                </h1>
-            </Link>
+                    <h1 className="gbk-whitespace-nowrap gbk-text-lg gbk-font-medium gbk-leading-none">
+                        {goldrushConfig.brand.title}
+                        <br />
+                        {goldrushConfig.brand.subtitle}
+                    </h1>
+                </Link>
+
+                {nativePrice && (
+                    <div className="gbk-whitespace-nowrap text-secondary-light dark:text-secondary-dark gbk-text-sm">
+                        <p className="text-foreground-light dark:text-foreground-dark">
+                            {
+                                nativePrice.contract_metadata
+                                    ?.contract_ticker_symbol
+                            }
+                            : <span>{nativePrice.pretty_price}</span>{" "}
+                            {delta !== null && (
+                                <span
+                                    className={
+                                        delta < 0
+                                            ? "text-danger"
+                                            : "text-success"
+                                    }
+                                >
+                                    {delta > 0 ? "+" : ""}
+                                    {delta}%
+                                </span>
+                            )}
+                        </p>
+
+                        <Timestamp timestamp={nativePrice.date} />
+                    </div>
+                )}
+            </div>
 
             <input
                 id="menu"
                 type="checkbox"
                 role="button"
                 onClick={() => setOpen(!open)}
-                className="gbk-ml-auto md:gbk-hidden"
+                className="gbk-ml-auto lg:gbk-hidden"
                 defaultChecked={open}
             />
 
             <div
                 className={`${
                     open ? "gbk-max-h-40" : "gbk-max-h-0 gbk-overflow-hidden"
-                } gbk-flex gbk-w-full gbk-items-center gbk-justify-between gbk-transition-all gbk-duration-500 gbk-ease-in-out md:gbk-max-h-fit md:gbk-flex-row`}
+                } gbk-flex gbk-w-full gbk-items-center gbk-justify-between gbk-transition-all gbk-duration-500 gbk-ease-in-out lg:gbk-max-h-fit lg:gbk-flex-row`}
             >
-                <div className="gbk-mt-4 gbk-flex gbk-flex-col gbk-items-center gbk-gap-2 md:gbk-mx-auto md:gbk-mt-0 md:gbk-flex-row">
+                <div className="gbk-mt-4 gbk-flex gbk-flex-col gbk-items-center gbk-gap-2 lg:gbk-mx-auto lg:gbk-mt-0 lg:gbk-flex-row">
                     <input
                         type="text"
                         name="search"
@@ -155,7 +257,9 @@ export const Navbar: React.FC = () => {
                     />
 
                     <ChainSelector
-                        onChangeChain={changeSelectedChainHandler}
+                        onChangeChain={(newChain) =>
+                            changeSelectedChainHandler(newChain, true)
+                        }
                         chain_options={goldrushConfig.chains}
                     />
                 </div>
