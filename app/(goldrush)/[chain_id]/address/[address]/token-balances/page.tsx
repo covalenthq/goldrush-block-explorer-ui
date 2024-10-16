3886be9f3c2a4ef601f5489251eb8baa619d90af
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2024-10-17 02:22:52 +0530

    fix: token balances chain names

--- app/(goldrush)/[chain_id]/address/[address]/token-balances/page.tsx
+++ app/(goldrush)/[chain_id]/address/[address]/token-balances/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { goldrushConfig } from "@/goldrush.config";
 import { type AddressPageProps } from "@/utils/types/pages.types";
-import { type Chain } from "@covalenthq/client-sdk";
 import { TokenBalancesList } from "@covalenthq/goldrush-kit";
 import Link from "next/link";
 
@@ -12,7 +10,7 @@ const AddressTokenBalancesPage: React.FC<AddressPageProps> = ({
     return (
         <TokenBalancesList
             address={address}
-            chain_names={goldrushConfig.chains as Chain[]}
+            chain_names={[chain_id]}
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-expect-error
             actionable_token={(address) => ({
