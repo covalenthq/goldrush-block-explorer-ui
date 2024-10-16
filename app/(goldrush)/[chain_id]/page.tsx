da9805cd84248c939701885fd2638972d0a2ec02
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2024-10-16 22:35:11 +0530

    fix: latest tx actionable address

--- app/(goldrush)/[chain_id]/page.tsx
+++ app/(goldrush)/[chain_id]/page.tsx
@@ -39,19 +39,10 @@ const Home: React.FC<HomePageProps> = ({ params: { chain_id } }) => {
                             chain_name={chain_id}
                             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
                             // @ts-expect-error
-                            actionable_from={(from__address) => ({
+                            actionable_address={(from__address) => ({
                                 parent: Link,
                                 parentProps: {
                                     href: `/${chain_id}/address/${from__address}`,
-                                    className: "hover:underline",
-                                },
-                            })}
-                            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                            // @ts-expect-error
-                            actionable_to={(to__address) => ({
-                                parent: Link,
-                                parentProps: {
-                                    href: `/${chain_id}/address/${to__address}`,
                                     className: "hover:underline",
                                 },
                             })}
